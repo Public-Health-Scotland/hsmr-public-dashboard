98ede7524922c5262ca6dac60e9f9a48c60bd592
catherineperkins <catherine.perkins@phs.scot>
2022-02-09 17:16:51 +0000

    Add funnel plot calcs

--- hsmr_data_prep.R
+++ hsmr_data_prep.R
@@ -1,27 +1,12 @@
-# Data prep - HSMR public dashboard
-# Output from RAP publication currently needs reconfigured to be more effective
+# Data prep for Shiny HSMR public dashboard
+# Output from HSMR publication currently needs reconfigured to be more effective
 # to be used as input for Shiny dashboard
 
-
-library(shiny)
-library(plotly)           # for charts
-library(shinyWidgets)     # for dropdowns
 library(dplyr)            # for data manipulation
-library(DT)               # for data table
-library(shinycssloaders)  # for loading icons, see line below
-# it uses github version devtools::install_github("andrewsali/shinycssloaders")
-# This is to avoid issues with loading symbols behind charts and perhaps with bouncing of app
-library(shinyjs)          # for enable/disable functions
 library(readr)            # for writing/reading csvs
 library(stringr)          # for manipulating strings
-library(flextable)
-library(shinyBS)          # for collapsible panels in commentary
-library(zoo)
-library(magrittr)
-library(shinymanager)
 library(lubridate)
-library(readxl)
-
+library(janitor)
 
 
 ###############################################.
@@ -53,8 +38,13 @@ pub_day <-lubridate::dmy(09112021)
 ## Data ----
 ###############################################.
 
-#smr <- read.csv(paste0(data_folder, pub_day, "/output/", pub_day, "_SMR-data_dashboard.csv"))
-trend <- read.csv((paste0(data_folder, pub_day, "/output/", pub_day, "_trends-data-level1.csv")), stringsAsFactors=FALSE)
+### TREND
+
+trend <- read_csv(paste0(data_folder, pub_day, "/output/", pub_day, "_trends-data-level1.csv"),
+                  col_types = cols(
+                    quarter = col_double(),
+                    quarter_short = col_character(),
+                    quarter_full = col_character()))
 
 
 #create scotland crude rate
@@ -66,10 +56,79 @@ trend %<>%
                                  time_period == "Month" ~ month_label),
          mth_qtr = case_when(time_period == "Quarter" ~ quarter,
                              time_period == "Month" ~ month)) %>%
-  select(-month, -month_label, -quarter, -quarter_short, -quarter_full) %>%
   select(hb, location, location_name, agg_label, time_period, mth_qtr, label_short,
          sub_grp, label, deaths, pats, crd_rate, scot_deaths, scot_pats,
          scot_crd_rate, completeness_date)
 
 saveRDS(trend, file = paste0("shiny_app/data/", pub_day, "-hsmr-trend-data.rds"))
 
+
+### HSMR
+
+# List of locations included in the Excel tables/dashboard files/markdown
+hosp_filter = c('A101H', 'A111H', 'A210H', 'B120H', 'D102H', 'F704H',
+                'G107H', 'C313H', 'G405H', 'C418H', 'H212H', 'H103H', 'C121H',
+                'H202H', 'L302H', 'L106H', 'L308H', 'N101H', 'N411H', 'R103H',
+                'S314H', 'S308H', 'S116H', 'T101H', 'T202H', 'T312H', 'V217H',
+                'W107H', 'Y146H', 'Y144H', 'Z102H')
+
+scot_filter = "Scot"
+
+hsmr <- read_csv(paste0(data_folder, pub_day, "/output/", pub_day, "_SMR-data.csv"))
+
+
+# create warning and control confidence limits for funnel plot
+
+hsmr %<>%
+  filter(period == 3 & location %in% c(hosp_filter, scot_filter)) %>%
+  mutate(st_err = round_half_up(sqrt(1/round_half_up(pred, 8)), 8),
+         z = if_else(location_type == "hospital",
+                     round_half_up(((round_half_up(smr, 8) - 1)/round_half_up(st_err,8)), 8),
+                     0)) %>%
+  mutate(
+    z_max = max(z),
+    z_min = min(z),
+    z_flag = case_when(z == z_max ~ 1,
+                       z == z_min ~ -1,
+                       TRUE ~ 0),
+    z = if_else(z == z_max | z == z_min, 0, z),
+    z_max = max(z),
+    z_min = min(z),
+    z = case_when(z_flag == 1 ~ z_max,
+                  z_flag == -1 ~ z_min,
+                  TRUE ~ z),
+    z_flag = if_else(z != 0, 1, 0),
+    w_score = round_half_up(sqrt(sum(round_half_up(z * z, 8))/sum(z_flag)),8)) %>%
+  # Calculate funnel limits for funnel plot
+  mutate(uwl = 1 + 1.96 * round_half_up(st_err * w_score,8),
+         ucl = 1 + 3.09 * round_half_up(st_err * w_score,8),
+         lwl = 1 - 1.96 * round_half_up(st_err * w_score,8),
+         lcl = 1 - 3.09 * round_half_up(st_err * w_score,8)) %>%
+
+  # Create flag for where hospital sits on funnel plot
+  mutate(flag = case_when(smr > ucl ~ "2",
+                          smr > uwl & smr <= ucl ~ "1",
+                          TRUE ~ "0"),
+         flag_above = case_when(smr > ucl ~ TRUE,
+                                TRUE ~ FALSE),
+         flag_below = case_when(smr < lcl ~ TRUE,
+                                TRUE ~ FALSE))
+
+
+hsmr %<>% select(hb, location, location_name, period_label, deaths, pred, pats, smr, crd_rate, smr_scot, death_scot, pats_scot,
+                 uwl, ucl, lwl, lcl, flag, flag_above, flag_below, completeness_date)
+
+# force control limits at upper and lower end - makes the lines look odd
+# hsmr %<>% mutate(lwl = case_when(lwl < 0 ~ 0,
+#                     TRUE ~ lwl),
+#                  lcl = case_when(lcl <0 ~ 0,
+#                                  TRUE ~ lcl),
+#                  uwl = case_when(uwl >2 ~ 2,
+#                                  TRUE ~ uwl),
+#                  ucl = case_when(ucl >2 ~2,
+#                                  TRUE ~ ucl))
+
+
+saveRDS(hsmr, file = paste0("shiny_app/data/", pub_day, "-hsmr-data.rds"))
+
+# END
