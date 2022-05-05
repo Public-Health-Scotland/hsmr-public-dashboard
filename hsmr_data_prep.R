3c0c4e68bed7944bdd952dc13e4ff1f786980992
catherineperkins <catherine.perkins@phs.scot>
2022-05-05 16:57:23 +0100

    Renamed subgroups and tidied code

--- hsmr_data_prep.R
+++ hsmr_data_prep.R
@@ -39,7 +39,6 @@ pub_day <-lubridate::dmy(09112021)
 ###############################################.
 
 ### TREND
-
 trend <- read_csv(paste0(data_folder, pub_day, "/output/", pub_day, "_trends-data-level1.csv"),
                   col_types = cols(
                     quarter = col_double(),
@@ -47,24 +46,35 @@ trend <- read_csv(paste0(data_folder, pub_day, "/output/", pub_day, "_trends-dat
                     quarter_full = col_character()))
 
 
-#create scotland crude rate
+# create scotland crude rate
 trend %<>% mutate(scot_crd_rate = (scot_deaths/scot_pats)*100)
 
-#create generic labels and remove redundant variables
+# create generic labels and remove redundant variables
 trend %<>%
   mutate(label_short = case_when(time_period == "Quarter" ~ quarter_short,
                                  time_period == "Month" ~ month_label),
          mth_qtr = case_when(time_period == "Quarter" ~ quarter,
-                             time_period == "Month" ~ month)) %>%
+                             time_period == "Month" ~ month),
+         sub_grp = case_when(sub_grp == "All Admissions" ~ "All admissions",
+                             sub_grp == "Admission Type" ~ "Admission type",
+                             sub_grp == "Age Group" ~ "Age group",
+                             sub_grp == "Place of Death" ~ "Place of death",
+                             TRUE ~ sub_grp),
+         label = case_when(label == "All Admissions" ~ "All admissions",
+                           label == "Admission Type" ~ "Admission type",
+                           label == "Age Group" ~ "Age group",
+                           label == "Place of Death" ~ "Place of death",
+                           TRUE ~ label)) %>%
   select(hb, location, location_name, agg_label, time_period, mth_qtr, label_short,
          sub_grp, label, deaths, pats, crd_rate, scot_deaths, scot_pats,
          scot_crd_rate, completeness_date)
+
+
 
 saveRDS(trend, file = paste0("shiny_app/data/", pub_day, "-hsmr-trend-data.rds"))
 
 
 ### HSMR
-
 # read data
 hsmr <- read_csv(paste0(data_folder, pub_day, "/output/", pub_day, "_SMR-data_dashboard.csv"))
 
@@ -77,10 +87,10 @@ hosp_filter = c('A101H', 'A111H', 'A210H', 'B120H', 'D102H', 'F704H',
 
 # Need to update the HB codes to be the 2019 codes - this could be added to RAP process
 hsmr %<>% mutate(hb = case_when(hb == "S08000018" ~ "S08000029",
-                           hb == "S08000027" ~ "S08000030",
-                           hb == "S08000021" ~ "S08000031",
-                           hb == "S08000023" ~ "S08000032",
-                           TRUE ~ hb))
+                                hb == "S08000027" ~ "S08000030",
+                                hb == "S08000021" ~ "S08000031",
+                                hb == "S08000023" ~ "S08000032",
+                                TRUE ~ hb))
 
 # Create a quarter number to enable sorting - this could be added to RAP process
 hsmr %<>% mutate(q_num = case_when(period_label == "April 2018 to March 2019" ~ 1,
@@ -102,20 +112,19 @@ hsmr %<>%
          z = if_else(location_type == "hospital",
                      round_half_up(((round_half_up(smr, 8) - 1)/round_half_up(st_err,8)), 8),
                      0)) %>%
-  mutate(
-    z_max = max(z),
-    z_min = min(z),
-    z_flag = case_when(z == z_max ~ 1,
-                       z == z_min ~ -1,
-                       TRUE ~ 0),
-    z = if_else(z == z_max | z == z_min, 0, z),
-    z_max = max(z),
-    z_min = min(z),
-    z = case_when(z_flag == 1 ~ z_max,
-                  z_flag == -1 ~ z_min,
-                  TRUE ~ z),
-    z_flag = if_else(z != 0, 1, 0),
-    w_score = round_half_up(sqrt(sum(round_half_up(z * z, 8))/sum(z_flag)),8)) %>%
+  mutate(z_max = max(z),
+         z_min = min(z),
+         z_flag = case_when(z == z_max ~ 1,
+                            z == z_min ~ -1,
+                            TRUE ~ 0),
+         z = if_else(z == z_max | z == z_min, 0, z),
+         z_max = max(z),
+         z_min = min(z),
+         z = case_when(z_flag == 1 ~ z_max,
+                       z_flag == -1 ~ z_min,
+                       TRUE ~ z),
+         z_flag = if_else(z != 0, 1, 0),
+         w_score = round_half_up(sqrt(sum(round_half_up(z * z, 8))/sum(z_flag)),8)) %>%
   # Calculate funnel limits for funnel plot
   mutate(uwl = 1 + 1.96 * round_half_up(st_err * w_score,8),
          ucl = 1 + 3.09 * round_half_up(st_err * w_score,8),
@@ -129,16 +138,10 @@ hsmr %<>%
                           smr <lwl & smr >= lcl ~ "4",
                           TRUE ~ "0"))
 
-
-         #  flag_above_ucl = case_when(smr > ucl ~ TRUE,
-         #                        TRUE ~ FALSE),
-         # flag_below_lcl = case_when(smr < lcl ~ TRUE,
-         #                        TRUE ~ FALSE))
-
-
-hsmr %<>% select(hb, location, location_name, q_num, period_label, deaths, pred, pats, smr, crd_rate, smr_scot, death_scot, pats_scot,
+# Keep only variables that are required for dashboard
+hsmr %<>% select(hb, location, location_name, q_num, period_label, deaths, pred,
+                 pats, smr, crd_rate, smr_scot, death_scot, pats_scot,
                  uwl, ucl, lwl, lcl, flag, completeness_date)
-
 
 saveRDS(hsmr, file = paste0("shiny_app/data/", pub_day, "-hsmr-data.rds"))
 
