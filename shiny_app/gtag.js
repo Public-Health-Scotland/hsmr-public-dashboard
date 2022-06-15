e0d51adc9ba08a55b459971bacf08c23e306b761
catherineperkins <catherine.perkins@phs.scot>
2022-06-15 11:28:41 +0100

    Added gtag file

---
+++ shiny_app/gtag.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+/*gtag('config', 'G-FST2NWF23R');*/
+gtag('config', 'G-FST2NWF23R', { 'anonymize_ip': true });
+
