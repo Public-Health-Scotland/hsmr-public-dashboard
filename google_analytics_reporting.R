bbd7e3cad7547a4fdcbadae43416c273affc7b49
Jaime Villacampa <jaime.villacampa@phs.scot>
2022-09-05 10:20:22 +0100

    updating for ga4 and hsmr

--- google_analytics_reporting.R
+++ google_analytics_reporting.R
@@ -18,28 +18,11 @@ library(janitor) #to clean names
 ## Lookups/filepaths ----
 ###############################################.
 
-data_folder <- "/conf/PHSCOVID19_Analysis/shiny_input_files/google_analytics/"
-
+data_folder <- "/conf/quality_indicators/hsmr/quarter_cycle/Dashboard/google_analytics/"
 # Lookup of event names and their equivalent tab name
-# list_events <- data.frame( stringsAsFactors = F,
-#                            eventlabel = c("apgar",  "booking", "breastfeeding", "cancer", 
-#                                           "cardio",  "child_dev", "child_health", 
-#                                           "comment", "drugs", "gestation", "imm", "inductions", "injuries", "intro", 
-#                                           "mentalhealth", "mod", "perinatal_mortality",  "preterm", 
-#                                           "sact", "summary", "table", "tears", "terminations"),
-#                            tabname = c("Apgar scores", "Antenatal bookings", "Breastfeeding", "Cancer pathology",
-#                                        "Cardiovascular", "Child development", "Child health reviews", "Commentary",
-#                                        "Substance use", "Gestation at delivery", "Immunisations", "Induction of labour", "Injuries", "Home page",
-#                                        "Mental health", "Mode of delivery", "Stillbirths and infant deaths",
-#                                        "Location of extremely preterm deliveries", "SACT", "Summary trends",
-#                                        "Data", "Perineal tears", "Termination of pregnancy"),
-#                            tabarea = c("Pregnancy/births/babies", "Pregnancy/births/babies", "Child health", 
-#                                        "Cancer", "Others", "Child health", "Child health", "Others",
-#                                        "Substance use", "Pregnancy/births/babies", "Child health", "Pregnancy/births/babies", "Others", "Others",
-#                                        "Others", "Pregnancy/births/babies", "Pregnancy/births/babies",
-#                                        "Pregnancy/births/babies", "Cancer", "Others",
-#                                        "Others", "Pregnancy/births/babies", "Pregnancy/births/babies"))
-# 
+list_events <- data.frame( stringsAsFactors = F,
+                           eventlabel = c( "hsmr", "fa", "home", "crude"),
+                           tabname = c("HSMR", "Further analysis", "Home", "Crude trends"))
 
 ###############################################.
 ## Connecting to GA and extracting data ----
@@ -49,18 +32,21 @@ data_folder <- "/conf/PHSCOVID19_Analysis/shiny_input_files/google_analytics/"
 # The library should then launch a browser window and ask you to login to Google - 
 # log in with an email that has access to your Google Analytics - it will take you 
 # to a screen with an OOB token. Copy-paste that token back into RStudio:
-ga_auth()
+# email_login is an object with the email address of the account with access to GA
+readRenviron(".env")
+email_login <- Sys.getenv("email_login")
+ga_auth(email = email_login)
 
 # See list of views/tables you have access to
 View(ga_account_list("ga4"))
 
 ## View account_list and pick the viewId you want to extract data from. 
-ga_id <- 319028171
+ga_id <- 319028171 
 start_date <- "2022-06-10"
 last_date <- paste0(Sys.Date())
 # last_year <- paste0(Sys.Date() - 365)
 
-View(ga_meta("data", propertyId = ga_id))
+# View(ga_meta("data", propertyId = ga_id))
 
 ## Query getting sessions data from first day we got data from to today
 sessions_ga <- ga_data(ga_id,
@@ -72,34 +58,34 @@ sessions_ga <- ga_data(ga_id,
 source_ga <- ga_data(ga_id,
                      date_range = c(start_date, last_date),
                      metrics = c("sessions", "averageSessionDuration"),
-                     dimensions = c('source','medium', "deviceCategory"))
+                     dimensions = c('sessionSource','sessionMedium', "deviceCategory"))
 
 ## Query to obtain country/city of users
 geo_ga <- ga_data(ga_id,
                   date_range = c(start_date, last_date),
                   metrics = c("sessions", "averageSessionDuration"),
-                  dimensions = c('city','region', 'metro', 'country'))
+                  dimensions = c('city','region', 'country'))
 
 ## Query getting events data from first day we got data from to today
 events_ga <- ga_data(ga_id,
                      date_range = c(start_date, last_date),
                      metrics = c("eventCount"),
-                     dimensions = c("date", "customEvent:event_label"))
+                     dimensions = c("date", "customEvent:event_label")) %>% 
+  clean_names() %>%  rename(eventlabel = custom_event_event_label)
 
-# metrics = c("customEvent:credits_spent"),
-# dimensions = c("date","customUser:last_level","customEvent:achievement_id"),
 ###############################################.
 ## Formatting data ----
 ###############################################.
 # Session data 
 
-# Calculating week endding and aggregating to obtain totals
+# Calculating week ending and aggregating to obtain totals
 sessions <- sessions_ga %>% 
-  mutate(week_ending = ceiling_date(date, "week", change_on_boundary = F)) %>% 
+  mutate(week_ending = ceiling_date(date, "week", change_on_boundary = F),
+         tot_session = sessions * averageSessionDuration) %>% 
   group_by(week_ending) %>% 
   summarise(count = sum(sessions, na.rm = T),
-            sessionDuration = sum(sessionDuration)) %>% ungroup() %>% 
-  mutate(session_ave = round(sessionDuration/count/60, 1))
+            tot_session = sum(tot_session, na.rm = T)) %>% ungroup() %>% 
+  mutate(session_ave = round(tot_session/count/60, 1))
 
 saveRDS(sessions, paste0(data_folder, "sessions.rds"))
 
@@ -110,37 +96,33 @@ saveRDS(sessions, paste0(data_folder, "sessions.rds"))
 events <- left_join(events_ga, list_events) 
 
 # Filtering out tab menus and not sets (these are internal links with no value)
-events %<>% 
-  filter(!eventlabel %in% c("(not set)", "Births and babies", "Cancer",
-                            "Child health", "Pregnancy", "intro"))
+events %<>% filter(!eventlabel %in% c("(not set)"))
 
 # Creating yearly totals
 events_year <- events %>% 
-  group_by(tabname, tabarea) %>% 
-  summarise(count = sum(totalEvents, na.rm = T)) %>% ungroup()
+  group_by(tabname) %>% 
+  summarise(count = sum(event_count, na.rm = T)) %>% ungroup()
 
 saveRDS(events_year, paste0(data_folder, "tabvisits_yearly.rds"))
 
 # Calculating week ending and aggregating to obtain totals
 events %<>% 
   mutate(week_ending = ceiling_date(date, "week", change_on_boundary = F)) %>% 
-  group_by(week_ending, tabname, tabarea) %>% 
-  summarise(count = sum(totalEvents, na.rm = T)) %>% ungroup()
-
-# Splitting data for each tab area to help with report/plotting
-events <- split.data.frame(events, events$tabarea)
+  group_by(week_ending, tabname) %>% 
+  summarise(count = sum(event_count, na.rm = T)) %>% ungroup()
 
 saveRDS(events, paste0(data_folder, "tabvisits.rds"))
 
 ###############################################.
 # Source data
 source_agg <- source_ga %>% 
-  group_by(source, medium) %>% 
+  mutate(tot_session = sessions * averageSessionDuration) %>% 
+  group_by(sessionSource, sessionMedium) %>% 
   summarise(count = sum(sessions),
-            sessionDuration = sum(sessionDuration)) %>% ungroup() %>% 
-  mutate(session_ave = round(sessionDuration/count/60, 1)) %>% 
-  slice_max(count, n = 20) %>% 
-  select(Source = source, Sessions = count, 
+            tot_session = sum(tot_session, na.rm = T)) %>% ungroup() %>% 
+  mutate(session_ave = round(tot_session/count/60, 1)) %>% 
+  # slice_max(count, n = 20) %>% # selecting only top 20
+  select(Source = sessionSource, Sessions = count, 
          "Average session length (minutes)" = session_ave)
 
 saveRDS(source_agg, paste0(data_folder, "source.rds"))
@@ -148,9 +130,10 @@ saveRDS(source_agg, paste0(data_folder, "source.rds"))
 ###############################################.
 # Device + session duration 
 device <- source_ga %>% 
+  mutate(tot_session = sessions * averageSessionDuration) %>% 
   group_by(deviceCategory) %>% 
   summarise(count = sum(sessions),
-            sessionDuration = sum(sessionDuration)) %>% ungroup() %>% 
+            sessionDuration = sum(tot_session)) %>% ungroup() %>% 
   mutate(session_ave = round(sessionDuration/count/60, 1),
          device = make_clean_names(deviceCategory, case = "title")) 
 
@@ -170,15 +153,17 @@ saveRDS(device, paste0(data_folder, "device.rds"))
 ###############################################.
 # Geography of users - Not adding this at the moment to the report
 city_top <- geo_ga %>% 
+  mutate(tot_session = sessions * averageSessionDuration) %>% 
   group_by(city) %>% 
   summarise(count = sum(sessions),
-            sessionDuration = sum(sessionDuration)) %>% ungroup() %>% 
+            sessionDuration = sum(tot_session)) %>% ungroup() %>% 
   mutate(session_ave = round(sessionDuration/count/60, 1)) 
 
 country_top <- geo_ga %>% 
+  mutate(tot_session = sessions * averageSessionDuration) %>% 
   group_by(country) %>% 
   summarise(count = sum(sessions),
-            sessionDuration = sum(sessionDuration)) %>% ungroup() %>% 
+            sessionDuration = sum(tot_session)) %>% ungroup() %>% 
   mutate(session_ave = round(sessionDuration/count/60, 1)) 
 
 ###############################################.
